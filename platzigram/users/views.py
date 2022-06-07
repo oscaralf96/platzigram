fc235e8ab160a3caea25659add0be453148f6c44
Oscar Gonzalez <racsogonzalez30@gmail.com>
2022-06-07 10:05:13 -0600

    forms validation

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render, redirect
 from datetime import datetime
 
 # Forms
-from .forms import ProfileForm
+from .forms import ProfileForm, SignupForm
 
 from .models import Profile
 
@@ -34,29 +34,18 @@ def login_view(request):
 def signup(request):
     """Sign up view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
 
 def update_profile(request):
